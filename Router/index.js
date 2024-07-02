96eb3b027aa1504949fc7b95b56a1adc561bc0d2
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2024-07-02 08:27:59 +0000

    logger create

--- Router/index.js
+++ Router/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../Controller/userController');
 const authMiddleware = require('../Middlewares/authMiddleware');
-
+router.get('/status',(req,res)=>{
+    return res.status(200).json({
+        message:"ok",
+    })
+})
 router.get('/process', authMiddleware, userController.processRequests);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('./Config/database');
 const redisClient = require('./Config/redis');
 const userRoutes = require('./Router');
+const cors = require('cors');
 
 const app = express();
 
+//middleware
 app.use(express.json());
+//router middleware
 app.use('/api/users', userRoutes);
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
