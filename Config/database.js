3d9075ba0b6f41f486f5bb9beabd9a7c0fa52658
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2024-07-02 08:06:14 +0000

    MongoDB issue fix

--- Config/database.js
+++ Config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/backend-system');
+mongoose.connect('mongodb+srv://gavaskaraeg:ZrO5vvaJJp9mKxZs@dnsmanager.peexu9o.mongodb.net/?retryWrites=true&w=majority&appName=dnsmanager');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
